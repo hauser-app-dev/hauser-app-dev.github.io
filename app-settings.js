e1aca04109b0b5a39c7c59ac10d4ce8d5e9ac729
phamaa <phamaa@quangpham.com>
2015-04-30 23:55:22 +0300

    Update app-settings.js

--- app-settings.js
+++ app-settings.js
@@ -3,9 +3,9 @@
         apiBaseUrl: function() {
             server_id = localStorage.getItem("HauserServerId");
             if (server_id == null) {
-                return "http://hauser-api-0.herokuapp.com/api/mobile/v1";
+                return "https://hauser-api-0.herokuapp.com/api/mobile/v1";
             } else {
-                return "http://hauser-api-" + server_id + ".herokuapp.com/api/mobile/v1";
+                return "https://hauser-api-" + server_id + ".herokuapp.com/api/mobile/v1";
             }
 
         },
