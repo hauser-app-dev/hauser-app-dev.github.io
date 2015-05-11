a8026279260c04f3133c7fd2ecc4ca82e0394361
Your Name <you@example.com>
2015-05-11 10:46:29 +0300

    ok

--- app-settings.js
+++ app-settings.js
@@ -11,7 +11,7 @@
         // firebaseUrl: "https://hauserdev.firebaseio.com",
 
         rootUrlState: "menu.feed",
-        unauthorizedFallbackUrlState: "landing"
+        unauthorizedFallbackUrlState: "landing.home"
     }
 })();
 
